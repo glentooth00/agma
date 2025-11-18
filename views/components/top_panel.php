06096ff3b5877a3dcdd39ef715aea7ff9ea134a9
Glen <glendonnalpasan1@gmail.com>
2025-11-18 11:32:50 +0800

    wip

--- views/components/top_panel.php
+++ views/components/top_panel.php
@@ -3,10 +3,12 @@
     <div class="dropdown">
         <i class="fa-solid fa-gear" id="gearIcon"></i>
         <div class="dropdown-menu">
-            <a href="#"><i class="fa-solid fa-sliders"></i> Settings</a>
+            <a href="#"><i class="fa-solid fa-sliders" data-toggle="modal" data-target="#exampleModal"></i> Settings</a>
             <a href="#"><i class="fa-solid fa-user"></i> Profile</a>
             <a href="../scripts/logout.php?userid=<?= $userid ?>"><i class="fa-solid fa-right-from-bracket"></i>
                 Logout</a>
         </div>
     </div>
 </div>
+
+
